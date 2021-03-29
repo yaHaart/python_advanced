b16ef6b1301df7b5364c8ed64f5da6c56bcfaf3a
haart <haart@yandex.ru>
2021-03-29 15:29:49 +0300

    hometask done

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,4 +1,5 @@
 import datetime
+from random import choice, randint
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,29 +7,52 @@ app = Flask(__name__)
 
 @app.route('/hello_world')
 def test_function():
-   pass
+    return 'Hello World!'
 
 
 @app.route('/cars')
-def test_function():
-   pass
+def cars():
+    cars = 'Chevrolet, Renault, Ford, Lada'
+    return cars
+
 
 
 @app.route('/cats')
-def test_function():
-   pass
+def cats():
+    cat = choice(['Корниш рекс', 'Русская голубая', 'Шотландская вислоухая', 'Мэйн-Кун', 'Манчкин'])
+    return cat
 
 
 @app.route('/get_time/now')
-def test_function():
-   pass
+def time_now():
+    return str(datetime.datetime.now())
 
 
 @app.route('/get_time/future')
-def test_function():
-   pass
+def time_future():
+    time_now = datetime.datetime.now()
+    time_future = time_now + datetime.timedelta(hours=1)
+    return str(time_future)
 
 
 @app.route('/get_random_word')
-def test_function():
-   pass
+def random_word():
+    with open('voyna-i-mir.txt', 'r', encoding='UTF-8') as file:
+        text = file.readlines()
+    random_line_number = randint(0, len(text))
+    random_line = text[random_line_number - 1]
+    random_word = choice(random_line.split())
+    return random_word
+
+
+@app.route('/counter')
+def counter():
+    global count
+    count += 1
+    return 'страница запускалась ' + str(count) + ' раз'
+
+count = 0
+if __name__ == "__main__":
+    app.run()
+
+
